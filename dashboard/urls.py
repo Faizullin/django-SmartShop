201da04a2ffb18448783d6cd0e23ed88c5950fc4
Faizullin <osmanfajzulla@gmail.com>
2023-03-07 23:27:36 +0600

    Osman save

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,12 +5,15 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from dashboard import views
-
+ 
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('shop/', views.shop_index, name='shop_index'),
+    path('shop/create', views.shop_index, name='shop_create'),
+    path('shop/update/<int:pk>', views.shop_index, name='shop_edit'),
+    
     # The home page
     path('', views.index, name='home'),
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,11 +5,13 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django import template
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
 from django.urls import reverse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from SmartShop.models import *
+from .forms import *
 
 LOGIN_URL = "/auth/login"
 
@@ -17,7 +19,7 @@ LOGIN_URL = "/auth/login"
 def index(request):
     context = {'segment': 'index'}
 
-    html_template = loader.get_template('home/index.html')
+    html_template = loader.get_template('dashboard/index.html')
     return HttpResponse(html_template.render(context, request))
 
 
@@ -34,21 +36,52 @@ def pages(request):
             return HttpResponseRedirect(reverse('admin:index'))
         context['segment'] = load_template
 
-        html_template = loader.get_template('home/' + load_template)
+        html_template = loader.get_template('dashboard/' + load_template)
         return HttpResponse(html_template.render(context, request))
 
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template('home/page-404.html')
+        html_template = loader.get_template('dashboard/page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     except:
-        html_template = loader.get_template('home/page-500.html')
+        html_template = loader.get_template('dashboard/page-500.html')
         return HttpResponse(html_template.render(context, request))
 
 @login_required(login_url=LOGIN_URL)
 def shop_index(request):
-    context = {'segment': 'shop_index'}
-
-    html_template = loader.get_template('home/shop/index.html')
+    shops = Shop.objects.all()
+    context = {
+        'segment': 'shop_index',
+        'shops': shops,
+        'form': ShopForm()
+    }
+    html_template = loader.get_template('dashboard/shop/index.html')
     return HttpResponse(html_template.render(context, request))
+
+@login_required(login_url=LOGIN_URL)
+def shop_create(request):
+    if request.method == 'POST':
+        form = ShopForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = ShopForm()
+    return render(request, 'shop_form.html', {'form': form})
+
+@login_required(login_url=LOGIN_URL)
+def shop_edit(request, pk):
+    shop = get_object_or_404(Shop, pk=pk)
+    if request.method == 'POST':
+        form = ShopForm(request.POST, instance=shop)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+    else:
+        form = ShopForm(instance=shop)
+    return render(request, 'shop_form.html', {'form': form})
